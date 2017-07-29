824db02e00aee5fa1ba447d7e0913f38dad6d637
Micah Shanks <micah.shanks@gmail.com>
2017-07-29 11:46:17 -0600

    cleaned code up, comments

--- get_housing_data.py
+++ get_housing_data.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
 from pandas.tools.plotting import scatter_matrix
-from sklearn.preprocessing import Imputer, StandardScaler, LabelBinarizer
+from sklearn.preprocessing import Imputer, StandardScaler, LabelBinarizer, LabelEncoder
 from attributesadder import CombinedAttributesAdder, DataFrameSelector
 from sklearn.pipeline import FeatureUnion, Pipeline
 
@@ -27,11 +27,6 @@ def load_housing_data():
     housing = pd.DataFrame.from_csv("data/housing.csv", index_col=None)
     housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
     housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
-
-    # housing["housing_per_household"] = housing["total_rooms"]/housing["households"]
-    # housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
-    # housing["population_per_household"] = housing["population"]/housing["households"]
-    # print(housing.head())
     return housing
 
 def hist_plot():
@@ -51,73 +46,81 @@ def strat_split():
 
 def lat_long_plot():
     #adding the alpha parameter identifies places of high density
-    housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+    strat_train_set.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
     # and now giving it more distinction
-    housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+    strat_train_set.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
         s=housing["population"]/100, label="population",
         c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
     plt.savefig("plots/lat_long.png")
 
 def corr_matrix():
-    corr_matrix = train_df.corr()
+    corr_matrix = strat_train_set.corr()
     mhv = corr_matrix["median_house_value"].sort_values(ascending=False)
     print(mhv)
     attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
     sm = scatter_matrix(housing[attributes], figsize=(10,6))
     plt.savefig("plots/scatter_matrix.png")
-    mi_sm = train_df.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+    mi_sm = strat_train_set.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
     #notice the lines that emerge at 500K, 450K, etc.
     #it might be a ghood idea to remove districts creating these lines
     #so the algorithm doesn't learn to produce these quirks
     plt.savefig("plots/scatter_mhv.png")
 
 def clean():
-    #creating a clean set to clean
-    # housing_tr = train_df.drop("median_house_value", axis=1)
-    # housing_labels = train_df["median_house_value"].copy()
-    # imputer = Imputer(strategy="median")
-    #making a copy as Imputer can only be used on numerial data
-    housing_num = housing.drop("ocean_proximity", axis=1)
-    #here the median will be used to fill missing values
-    # imputer.fit(housing_num)
-    # #producing a numpy array
-    # X = imputer.transform(housing_num)
-    # housing_tr = pd.DataFrame(X, columns=housing_num.columns)
-
     num_attribs = list(housing_num)
     cat_attribs = ["ocean_proximity"]
 
+    '''
+    handle pandas df with DataFrameSelector class
+    imputing all missing numerical values with the median
+    adding rooms per household, population per household, and bedrooms per rooms
+    standardizing data
+    '''
     num_pipeline = Pipeline([
             ('selector', DataFrameSelector(num_attribs)),
             ('imputer', Imputer(strategy="median")),
             ('attribs_adder', CombinedAttributesAdder()),
             ('std_scaler', StandardScaler()),
         ])
-
+    '''
+    LabelBinarizer transforms text categories to integar categories and then from int categories to one-hot vectors
+    '''
     cat_pipeline = Pipeline([
             ('selector', DataFrameSelector(cat_attribs)),
             ('label_binarizer', LabelBinarizer()),
         ])
 
+    #has to be a better way to get the encoded columns...
+    # encoder = LabelEncoder()
+    # housing_cat = housing["ocean_proximity"]
+    # housing_cat_enc = encoder.fit_transform(housing_cat)
+    #
+    # # print(encoder.classes_)
+
+    #piplines handling numerical and categorical combined
     full_pipeline = FeatureUnion(transformer_list=[
         ("num_pipeline", num_pipeline),
         ("cat_pipeline", cat_pipeline),
     ])
 
     housing_prep = full_pipeline.fit_transform(housing)
-    #and then putting the numpy array back into a dataframe
-    # housing_tr = pd.DataFrame(X, columns=housing_num_columns)
     return housing_prep
 
 if __name__ == '__main__':
+    plt.close('all')
     #fetch_housing_data()
     housing = load_housing_data()
     # hist_plot()
     train_set, test_set = random_split()
     strat_train_set, strat_test_set = strat_split()
-    #for EDA
-    housing = strat_train_set.drop("median_house_value", axis=1)
-    housing_labels = strat_train_set["median_house_value"].copy()
+    #plots using strat train set
     # lat_long_plot()
     # corr_matrix()
-    housing_prepared = clean()
+    #for pipeline
+    house_train = strat_train_set.drop("median_house_value", axis=1)
+    housing_num = house_train.drop("ocean_proximity", axis=1)
+    num_attribs = list(housing_num)
+    cat_attribs = ["ocean_proximity"]
+    #prepare training data with pipeline, exact pipeline should be used for
+    #test data later
+    housing_prep = clean()
